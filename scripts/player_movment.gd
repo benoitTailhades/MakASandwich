c986d832f6c6ea2a50c550d7524ef0c7527e6883
D0mano <njfotso@gmail.com>
2026-04-20 23:16:00 +0200

    feat: add Player idle animation

--- scripts/player_movment.gd
+++ scripts/player_movment.gd
@@ -6,7 +6,7 @@ var character_direction : String  = "Down"
 
 func _physics_process(delta: float) -> void:
 	player_movement()
-	player_animation()
+	
 
 
 func player_movement():
@@ -14,22 +14,28 @@ func player_movement():
 		character_direction = "Right"
 		velocity.x = mouvment_speed
 		velocity.y = 0
+		%sprite.animation = character_direction
+		
 	elif Input.is_action_pressed("move_left"):
 		character_direction = "Left"
 		velocity.x = - mouvment_speed
 		velocity.y = 0
+		%sprite.animation = character_direction
 	elif Input.is_action_pressed("move_up"):
 		character_direction = "Up"
 		velocity.y = -mouvment_speed
 		velocity.x = 0
+		%sprite.animation = character_direction
 	elif Input.is_action_pressed("move_down"):
 		character_direction = "Down"
 		velocity.y = mouvment_speed
 		velocity.x = 0
+		%sprite.animation = character_direction
 	else:
 		velocity.y = 0
 		velocity.x = 0
+		%sprite.animation = character_direction+"-Idle"
 	move_and_slide()
 
-func player_animation():
-	%sprite.animation = character_direction
+
+		

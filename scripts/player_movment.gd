cd0c3104085048ee5a05e05bc26196c544bd6983
D0mano <njfotso@gmail.com>
2026-04-21 11:27:43 +0200

    feat: Add state machine

--- scripts/player_movment.gd
+++ scripts/player_movment.gd
@@ -1,64 +1,6 @@
+class_name Player
 extends CharacterBody2D
 
-@export var mouvment_speed : float = 100
-var character_direction : String = "Down"
+
+var player_direction : Vector2 
 @export var inv: Inv
-
-# Limites à définir depuis world.gd
-var limit_left : float = -480
-var limit_right : float = 2832
-var limit_top : float = -976
-var limit_bottom : float = 320
-
-func _physics_process(delta: float) -> void:
-	player_movement()
-	
-
-func player_movement():
-	if Input.is_action_pressed("move_right"):
-		character_direction = "Right"
-		velocity.x = mouvment_speed
-		velocity.y = 0
-		%sprite.animation = character_direction
-		
-	elif Input.is_action_pressed("move_left"):
-		character_direction = "Left"
-		velocity.x = -mouvment_speed
-		velocity.y = 0
-		%sprite.animation = character_direction
-	elif Input.is_action_pressed("move_up"):
-		character_direction = "Up"
-		velocity.y = -mouvment_speed
-		velocity.x = 0
-		%sprite.animation = character_direction
-	elif Input.is_action_pressed("move_down"):
-		character_direction = "Down"
-		velocity.y = mouvment_speed
-		velocity.x = 0
-		%sprite.animation = character_direction
-	else:
-		velocity.y = 0
-		velocity.x = 0
-<<<<<<< HEAD
-
-	move_and_slide()
-
-	position.x = clamp(position.x, limit_left, limit_right)
-	position.y = clamp(position.y, limit_top, limit_bottom)
-
-=======
-		%sprite.animation = character_direction+"-Idle"
-	move_and_slide()
-
-
-<<<<<<< HEAD
-=======
-		
-=======
->>>>>>> 914cee875612a572ad5dd1d7ecc399b5adf28dce
->>>>>>> 8fba516b3ffb7e7dd217a2ba5b3848fa42829467
-func collect(item):
-	inv.insert(item)
-
-func player_animation():
-	%sprite.animation = character_direction
---
+++ scripts/state_machine/walk_state.gd
@@ -0,0 +1,51 @@
+ 
+extends NodeState
+
+@export var player : Player
+@export var animated_sprite_2d : AnimatedSprite2D
+@export var speed: int = 100
+
+var direction : Vector2
+var limit_left : float = -480
+var limit_right : float = 2832
+var limit_top : float = -976
+var limit_bottom : float = 320
+
+func _on_process(_delta : float) -> void:
+	pass
+
+
+func _on_physics_process(_delta : float) -> void:
+	var direction: Vector2 = GameInputEvents.movement_input()
+	
+	if direction == Vector2.UP:
+		animated_sprite_2d.play("Up")
+	elif direction == Vector2.DOWN:
+		animated_sprite_2d.play("Down")
+	elif direction == Vector2.LEFT:
+		animated_sprite_2d.play("Left")
+	elif direction == Vector2.RIGHT:
+		animated_sprite_2d.play("Right")
+		
+	if direction != Vector2.ZERO:
+		player.player_direction = direction 
+		
+	player.velocity = direction * speed
+	player.position.x = clamp(player.position.x,limit_left,limit_right)
+	player.position.y = clamp(player.position.y,limit_top,limit_bottom)
+	player.move_and_slide()
+	
+
+
+func _on_next_transitions() -> void:
+	GameInputEvents.movement_input()
+	if !GameInputEvents.is_movement_input():
+		transition.emit("Idle")
+
+
+func _on_enter() -> void:
+	pass
+
+
+func _on_exit() -> void:
+	animated_sprite_2d.stop()
